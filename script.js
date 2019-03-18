2f8d10b4ea168754590eaba7066db86c91c94960
Osky772 <kowalow.oskar@gmail.com>
2019-03-18 21:24:09 +0100

    Render avatar and name of user

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ let userGhData = {};
 async function getUserData(nick) {
 	const response = await fetch(`https://api.github.com/users/${nick}`);
 	const data = await response.json();
-	console.log(data);
+	userGhData = data;
+	fillUserHeader(userGhData);
 }
+
+const fillUserHeader = data => {
+	const userName = document.querySelector("#profile__name");
+	const userAvatar = document.querySelector("#profile__avatar img");
+	userName.textContent = data.name;
+	userAvatar.src = data["avatar_url"];
+
+	console.log(data);
+};
 
 findUserForm.addEventListener("submit", function(e) {
 	e.preventDefault();
