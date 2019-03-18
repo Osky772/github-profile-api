0b783e0371a2c523c48ba0400470cdf414d3de7b
Osky772 <kowalow.oskar@gmail.com>
2019-03-18 21:14:28 +0100

    Get user data and log in console

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const getUserInput = document.querySelector("#gh-username");
 const sendButton = document.querySelector("#send");
 const findUserForm = document.querySelector("#find-user");
 
-getUserInput.addEventListener("keyup", function(e) {
-	console.log(e.target.value);
-});
+let userGhData = {};
+
+async function getUserData(nick) {
+	const response = await fetch(`https://api.github.com/users/${nick}`);
+	const data = await response.json();
+	console.log(data);
+}
 
 findUserForm.addEventListener("submit", function(e) {
 	e.preventDefault();
+	const userNickname = getUserInput.value;
+	getUserData(userNickname);
 });
